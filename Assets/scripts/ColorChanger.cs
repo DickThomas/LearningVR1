96ec9c511c3998dbaeb616198f4fdb93e162ee71
Dick Thomas <822758+DickThomas@users.noreply.github.com>
2020-10-10 07:07:19 +0100

    Hello World
    At this point Ive got a working world and "hands" with a  few items infront ont me

---
+++ Assets/scripts/ColorChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+  public void setColorRed()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+        
+    }
+
+  public void setColourBlue()
+    {
+
+        GetComponent<MeshRenderer>().material.color = Color.green;
+
+
+    }
+}
+
